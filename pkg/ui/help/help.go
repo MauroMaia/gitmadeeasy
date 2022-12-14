3566b39b3e6e7ce885d77e07f590f7862adf1163
Mauro Maia <dev@maurofilipemaia.dev>
2022-12-14 23:15:40 +0000

    fix(ui):Add missing help message to files changed page

--- pkg/ui/help/help.go
+++ pkg/ui/help/help.go
@@ -9,6 +9,7 @@ import (
 
 const HELP_TEXT_MENU = "KeyArrow Down/UP Slect menu option | Enter select Option"
 const HELP_TEXT_LIST = "KeyArrow Down/UP to scroll | Press Crontol + space to go back to menu"
+const HELP_TEXT_FILE_CHANGED = "KeyArrow Down/UP to scroll | Press Crontol + S to add file | Press Crontol + X write commit"
 const HELP_TEXT_DEFAULT = "This view(%s) does not yet have a help information. More at: https://github.com/MauroMaia/gitmadeeasy"
 
 func LayoutShowHelpView(g *gocui.Gui, xBegins int, yBegins int) *gocui.View {
@@ -32,6 +33,10 @@ func LayoutShowHelpView(g *gocui.Gui, xBegins int, yBegins int) *gocui.View {
 	case constants.COMMIT_LIST_VIEW:
 		_, _ = fmt.Fprintln(v, HELP_TEXT_LIST)
 		break
+	case constants.FILE_CHANGED_VIEW:
+		_, _ = fmt.Fprintln(v, HELP_TEXT_FILE_CHANGED)
+		break
+
 	default:
 		_, _ = fmt.Fprintf(v, HELP_TEXT_DEFAULT, constants.HIGHLIGHT_VIEW)
 	}
